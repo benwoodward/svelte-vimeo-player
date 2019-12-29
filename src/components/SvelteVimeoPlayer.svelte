f71e88d98b587e6495282aafa82132f4530bf534
Joe Woodward <joe@oozou.com>
2019-12-29 13:40:04 +0700

    Refactor event dispatching for Svelte framework and remove calls to ‘this’

--- src/components/SvelteVimeoPlayer.svelte
+++ src/components/SvelteVimeoPlayer.svelte
@@ -2,7 +2,7 @@
 </div>
 
 <script>
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount, onDestroy, createEventDispatcher } from 'svelte';
 
   import Player from '@vimeo/player';
   import assign from 'object-assign';
@@ -14,17 +14,12 @@
   export let loop = false;
   export let autoplay = false;
 
+  const dispatch = createEventDispatcher();
   let playerId = parseInt(Math.random() * 100000).toString();
   let elementId = `vimeo-player-${videoId}`;
   let player;
 
   $: loadVideo(videoId)
-
-  function emitVueEvent (event) {
-    this.player.on(event, (data) => {
-      this.$emit(event, data, this.player)
-    })
-  }
 
   const eventsToEmit = [
     'play',
@@ -42,54 +37,63 @@
   ]
 
   function loadVideo(id) {
-    return this.player.loadVideo(id)
+    if (player != undefined) {
+      return player.loadVideo(id)
+    }
   }
 
   function play() {
-    return this.player.play()
+    return player.play()
   }
 
   function pause() {
-    return this.player.pause()
+    return player.pause()
   }
 
   function mute() {
-    return this.player.setVolume(0)
+    return player.setVolume(0)
   }
 
   function unmute(volume = 0.5) {
-    return this.player.setVolume(volume)
+    return player.setVolume(volume)
   }
 
   function setEvents() {
-    const vm = this
-
-    this.player.ready()
+    player.ready()
       .then(function () {
-        vm.$emit('ready', vm.player)
+        dispatch('ready', { player: player })
       })
       .catch((error) => {
-        vm.$emit('error', error, vm.player)
+        dispatch('error', { error: error, player: player })
       })
 
-    eventsToEmit.forEach(event => emitVueEvent.call(vm, event))
+    eventsToEmit.forEach(event => emitSvelteEvent.call(player, event))
+  }
+
+  function emitSvelteEvent (event) {
+    player.on(event, (data) => {
+      dispatch(event, {
+        data: data,
+        player: player
+      })
+    })
   }
 
   onMount(async () => {
     const options = {
-      id: this.videoId,
-      width: this.playerWidth,
-      height: this.playerHeight,
-      loop: this.loop,
-      autoplay: this.autoplay
+      id: videoId,
+      width: playerWidth,
+      height: playerHeight,
+      loop: loop,
+      autoplay: autoplay
     }
 
-    this.player = new Player(this.elementId, assign(options, this.options))
+    player = new Player(elementId, assign(options, options))
 
-    this.setEvents()
+    setEvents()
   });
 
 	onDestroy(() => {
-    this.player.unload()
+    player.unload()
   });
 </script>
