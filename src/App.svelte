4ff94b5a5cad608199bf4888e8a65dd10abee759
Joe Woodward <joe@oozou.com>
2019-12-29 13:37:45 +0700

    Log events in App.svelte for testing

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,28 @@
 <script>
   import { SvelteVimeoPlayer } from './components/components.module.js';
+
+  function logEvent(event) {
+    console.log(event)
+  }
 </script>
 
 <style>
-
 </style>
 
-test
-<SvelteVimeoPlayer videoId="54348266" />
+<SvelteVimeoPlayer
+  videoId="54348266"
+  on:ready={logEvent}
+  on:error={logEvent}
+  on:play={logEvent}
+  on:pause={logEvent}
+  on:ended={logEvent}
+  on:timeupdate={logEvent}
+  on:progress={logEvent}
+  on:seeked={logEvent}
+  on:texttrackchange={logEvent}
+  on:cuechange={logEvent}
+  on:cuepoint={logEvent}
+  on:volumechange={logEvent}
+  on:error={logEvent}
+  on:loaded={logEvent}
+/>
